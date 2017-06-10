191176697354351ba969c89bafab310888dbaf54
Sowmya R <sowmyas@baasha.net>
2017-06-09 23:01:34 -0400

    Add files via upload

--- app/view2/view2.module.js
+++ app/view2/view2.module.js
@@ -4,7 +4,7 @@ angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
-  	title : 'Home',
+  	title : 'View2',
 	robots : 'follow,index',
 	description : 'This site is a single page AngularJS app. It\'s intended to test whether search engines can crawl and adequately render & index the angular pages',
 	canonical : 'http://angular.sowmya.samples.com/view2',
